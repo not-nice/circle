58ad46e5f41e4ac9f1f6cad272fdc904ddcee6dc
not-nice <85105948+not-nice@users.noreply.github.com>
2024-02-13 17:36:33 +0100

    terraform apply

--- main.tf
+++ main.tf
@@ -9,9 +9,9 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"  # Change this to your desired AWS region
-  access_key = "ASIARL7AWOA4XW4CRPLY"
-  secret_key = "QXQAKcR7M/eTht5wOcT+N5C4hGMOh9rYHeiAR/5p"
-  token      = "FwoGZXIvYXdzEJ7//////////wEaDKg5yTF8uKCg1ePjCiLXAXS8JsVjDJ1IndchUnMAsJsVKbwf25jta8UgTvMPxrgLS0ITaNWbUY2s8OEXBgmYToaEQq/7Iucs7mBFOxOgnWIVi31NnFR/ywEK4GSxCwKgdFQxwU8x8fFvUcA0/sAe6BeF8ptneYW+92Dcrk58cqQyKGlgSaeCzZojAFmxYju7goSG6Ki3uJ0PKOhwK6GDirkHSGku0++Qn5LR9C6mC7lSvR8l7ZekRv1T9nZleziCojV+QKgWgXB34CsPOv594KsQ7oYcpVE1BAL6U8W59W4M1YmTsC/gKMr+qa4GMi1fujEMwwbJ8k5cejkill9nFiLHJdjd8PCw28tDwdNVHAzAi/JQ0Jx+U3n3API="
+  access_key = "ASIARL7AWOA47OIDULX3"
+  secret_key = "zpa0/oTM3Fwqk4COo8Zg702cpW5qqhhmeIBYDy17"
+  token      = "FwoGZXIvYXdzELL//////////wEaDDtUlJcLBVnZfZk6ViLXAVQ2kng80g2u9A6io9JKCxEcEW1goHiCnXpKnEjtYZexjZ7q7c+zoG+l+m3IvJH9oWylajp8tQdHCE8hL2n9JgjXQwpIzNkE7fM+DdnpReakd9EWR+ijfKbb3HmEqHja+D6ho+Hxuwo1xwoRsFd9gLshv6s2CdFMmwS2EnIspDiPgPmM4fwV/p80efwSKvHXgB8VvdBolNCezTCJVdDYJDXEPRZATp+AO+crTdL8VAp30I1u3yqrfvF7kcfvTcTvUNLKArFbqCMvkhqRBNJ4JHi8UDgAREx9KJSprq4GMi0A9/JNpLlJSOGTcpght7kBprmLAxMOLvRuTgf++kssISfbhXmW7KlSulLwMQ4="
 }
 
 # Create the VPC
@@ -27,6 +27,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"  # CIDR block for the public subnet
+  map_public_ip_on_launch = true  # Enable automatic assignment of public IP addresses
 
   tags = {
     Name = "PublicSubnet"
@@ -43,13 +44,27 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
+# Set up RDS instance
+resource "aws_db_instance" "my_database" {
+  engine         = "mysql"
+  instance_class = "db.t2.micro"
+  allocated_storage = 20
+  identifier     = "my-database56-instance"
+  username       = "admin"
+  password       = "admin"
+}
+
 # Resource Definitions
 resource "aws_instance" "Instance-1" {
   ami           = "ami-0c7217cdde317cfec"  # Change this to a valid AMI ID
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id  # Use the ID of the public subnet
-}
+  associate_public_ip_address = true  # Assign a public IPv4 address to the instance
 
+  tags = {
+    Name = "MyInstance"
+  }
+}
 
 # Define a security group for the EC2 instance
 resource "aws_security_group" "public_ec2_sg" {
